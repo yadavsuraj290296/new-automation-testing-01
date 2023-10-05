8e8c64942addb2ed129f7d3ba06659a25e35c6b7
suraj <suraj@LAPTOP-62C0PDQP>
2023-10-05 22:17:14 +0530

    send to staged area to local repository

---
+++ medisolve/src/packege/class2.java
@@ -0,0 +1,9 @@
+package packege;
+
+public class class2 {
+	public static void main(String[] args) {
+	System.out.println("it is automation testing");
+		System.out.println("new classes added");
+	}
+
+}
---
+++ medisolve/src/packege/class3.java
@@ -0,0 +1,5 @@
+package packege;
+
+public class class3 {public static void main(String[] args) {
+	System.out.println();
+}}
